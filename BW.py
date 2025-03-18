877718339681266ae80b6c2348ea337b9d97efbc
xlx <xlx@sjtu.edu.cn>
2025-03-18 22:55:40 +0800

    V0.7 -remote work

---
+++ BW.py
@@ -0,0 +1,267 @@
+import json
+import os
+import time
+import pyaudio
+import sys
+import boto3
+
+from amazon_transcribe.client import TranscribeStreamingClient
+from amazon_transcribe.handlers import TranscriptResultStreamHandler
+from amazon_transcribe.model import TranscriptEvent, TranscriptResultStream
+
+from api_request_schema import api_request_list, get_model_ids
+from TextInputApp import app
+
+model_id = os.getenv('MODEL_ID', 'anthropic.claude-3-sonnet-20240229-v1:0')
+aws_region = os.getenv('AWS_REGION', 'us-east-1')
+
+if model_id not in get_model_ids():
+    print(f'Error: Models ID {model_id} in not a valid model ID. Set MODEL_ID env var to one of {get_model_ids()}.')
+    sys.exit(0)
+
+api_request = api_request_list[model_id]
+config = {
+    'log_level': 'info',  # One of: info, debug, none
+    #'last_speech': "If you have any other questions, please don't hesitate to ask. Have a great day!",
+    'region': aws_region,
+    'polly': {
+        'Engine': 'neural',
+        'LanguageCode': 'cmn-CN',
+        'VoiceId': 'Zhiyu',
+        'OutputFormat': 'pcm',
+    },
+    'bedrock': {
+        'api_request': api_request
+    }
+}
+
+
+p = pyaudio.PyAudio()
+bedrock_runtime = boto3.client(service_name='bedrock-runtime', region_name=config['region'])
+polly = boto3.client('polly', region_name=config['region'])
+transcribe_streaming = TranscribeStreamingClient(region=config['region'])
+
+def printInfo():
+    ## 输出信息文本
+    info_text = f'''
+    *************************************************************
+    [INFO] Supported FM models: {get_model_ids()}.
+    [INFO] Change FM model by setting <MODEL_ID> environment variable. Example: export MODEL_ID=meta.llama2-70b-chat-v1
+
+    [INFO] AWS Region: {config['region']}
+    [INFO] Amazon Bedrock model: {config['bedrock']['api_request']['modelId']}
+    [INFO] Polly config: engine {config['polly']['Engine']}, voice {config['polly']['VoiceId']}
+    [INFO] Log level: {config['log_level']}
+
+    [INFO] Hit ENTER to interrupt Amazon Bedrock. After you can continue speaking!
+    [INFO] Go ahead with the voice chat with Amazon Bedrock!
+    *************************************************************
+    '''
+    print(info_text) 
+
+def printer(text, level):
+    if config['log_level'] == 'info' and level == 'info':
+        print(text)
+    elif config['log_level'] == 'debug' and level in ['info', 'debug']:
+        print(text)
+
+class BedrockModelsWrapper:
+
+    @staticmethod
+    def define_body(text, data=[]):
+        model_id = config['bedrock']['api_request']['modelId']
+        model_provider = model_id.split('.')[0]
+        body = config['bedrock']['api_request']['body']
+
+        if model_provider == 'amazon':
+            body['inputText'] = text
+        elif model_provider == 'meta':
+            if 'llama3' in model_id:
+                body['prompt'] = f"""
+                    <|begin_of_text|>
+                    <|start_header_id|>user<|end_header_id|>
+                    {text}, please output in Chinese.
+                    <|eot_id|>
+                    <|start_header_id|>assistant<|end_header_id|>
+                    """
+            else: 
+                body['prompt'] = f"<s>[INST] {text}, please output in Chinese. [/INST]"
+        elif model_provider == 'anthropic':
+            if "claude-3" in model_id:
+                content = [
+                    {
+                        "type": "text",
+                        "text": text
+                    }
+                ]
+                for image in data:
+                    content.append({
+                        "type": "image",
+                        "source": {
+                            "type": "base64",
+                            "media_type": "image/" + image[1][1:],
+                            "data": image[0]
+                        },
+                    })
+                body['messages'] = [
+                    {
+                        "role": "user",
+                        "content": content
+                    }
+                ]
+            else:
+                body['prompt'] = f'\n\nHuman: {text}\n\nAssistant:'
+        elif model_provider == 'cohere':
+            body['prompt'] = text
+        elif model_provider == 'mistral':
+            body['prompt'] = f"<s>[INST] {text}, please output in Chinese. [/INST]"
+        else:
+            raise Exception('Unknown model provider.')
+
+        return body
+
+    @staticmethod
+    def get_stream_chunk(event):
+        return event.get('chunk')
+
+    @staticmethod
+    def get_stream_text(chunk):
+        model_id = config['bedrock']['api_request']['modelId']
+        model_provider = model_id.split('.')[0]
+
+        chunk_obj = ''
+        text = ''
+        if model_provider == 'amazon':
+            chunk_obj = json.loads(chunk.get('bytes').decode())
+            text = chunk_obj['outputText']
+        elif model_provider == 'meta':
+            chunk_obj = json.loads(chunk.get('bytes').decode())
+            text = chunk_obj['generation']
+        elif model_provider == 'anthropic':
+            if "claude-3" in model_id:
+                chunk_obj = json.loads(chunk.get('bytes').decode())
+                if chunk_obj['type'] == 'message_delta':
+                    print(f"\nStop reason: {chunk_obj['delta']['stop_reason']}")
+                    print(f"Stop sequence: {chunk_obj['delta']['stop_sequence']}")
+                    print(f"Output tokens: {chunk_obj['usage']['output_tokens']}")
+
+                if chunk_obj['type'] == 'content_block_delta':
+                    if chunk_obj['delta']['type'] == 'text_delta':
+                        #print(chunk_obj['delta']['text'], end="")
+                        text = chunk_obj['delta']['text']
+            else:
+                #Claude2.x
+                chunk_obj = json.loads(chunk.get('bytes').decode())
+                text = chunk_obj['completion']
+        elif model_provider == 'cohere':
+            chunk_obj = json.loads(chunk.get('bytes').decode())
+            text = ' '.join([c["text"] for c in chunk_obj['generations']])
+        elif model_provider == 'mistral':
+            chunk_obj = json.loads(chunk.get('bytes').decode())
+            text = chunk_obj['outputs'][0]['text']
+        else:
+            raise NotImplementedError('Unknown model provider.')
+
+        printer(f'[DEBUG] {chunk_obj}', 'debug')
+        return text
+
+
+def to_audio_generator(bedrock_stream):
+    prefix = ''
+
+    if bedrock_stream:
+        for event in bedrock_stream:
+            chunk = BedrockModelsWrapper.get_stream_chunk(event)
+            if chunk:
+                text = BedrockModelsWrapper.get_stream_text(chunk)
+                # 注意到一个问题:这里的符号默认是英文的点哦，虽然没有怎么感觉到影响
+                if '.' in text:
+                    a = text.split('.')[:-1]
+                    to_polly = ''.join([prefix, '.'.join(a), '. '])
+                    prefix = text.split('.')[-1]
+                    print(to_polly, flush=True, end='')
+                    yield to_polly
+                else:
+                    prefix = ''.join([prefix, text])
+
+        if prefix != '':
+            print(prefix, flush=True, end='')
+            yield f'{prefix}.'
+
+        print('\n')
+
+
+class BedrockWrapper:
+
+    def __init__(self):
+        self.speaking = False
+
+    def is_speaking(self):
+        return self.speaking
+
+    def invoke_bedrock(self, text, data=[]):
+        printer('[DEBUG] Bedrock generation started', 'debug')
+        self.speaking = True
+
+        printer(f'[INFO] {text},{[type[1] for type in data]}', 'info')
+        body = BedrockModelsWrapper.define_body(text, data)
+        print(body)
+        printer(f"[DEBUG] Request body: {body}", 'debug')
+
+        try:
+            body_json = json.dumps(body)
+            response = bedrock_runtime.invoke_model_with_response_stream(
+                body=body_json,
+                modelId=config['bedrock']['api_request']['modelId'],
+                accept=config['bedrock']['api_request']['accept'],
+                contentType=config['bedrock']['api_request']['contentType']
+            )
+
+            printer('[DEBUG] Capturing Bedrocks response/bedrock_stream', 'debug')
+            bedrock_stream = response.get('body')
+            printer(f"[DEBUG] Bedrock_stream: {bedrock_stream}", 'debug')
+
+            audio_gen = to_audio_generator(bedrock_stream) # generator
+            printer('[DEBUG] Created bedrock stream to audio generator', 'debug')
+
+            for audio in audio_gen:
+                app.root.after(0,app.put_output,audio)
+
+        except Exception as e:
+            print(e)
+            time.sleep(2)
+            self.speaking = False
+
+        time.sleep(1)
+        self.speaking = False
+        printer('\n[DEBUG] Bedrock generation completed', 'debug')
+
+
+class Reader:
+
+    def __init__(self):
+        self.polly = boto3.client('polly', region_name=config['region'])
+        self.audio = p.open(format=pyaudio.paInt16, channels=1, rate=16000, output=True)
+        self.chunk = 1024
+
+    def read(self, data):
+        response = self.polly.synthesize_speech(
+            Text=data,
+            Engine=config['polly']['Engine'],
+            LanguageCode=config['polly']['LanguageCode'],
+            VoiceId=config['polly']['VoiceId'],
+            OutputFormat=config['polly']['OutputFormat'],
+        )
+
+        stream = response['AudioStream']
+
+        while True:
+            data = stream.read(self.chunk)
+            self.audio.write(data)
+            if not data:
+                break
+
+    def close(self):
+        time.sleep(1)
+        self.audio.stop_stream()
+        self.audio.close()
